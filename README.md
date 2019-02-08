f4381d33fd1dc05fe1bb6e0f7741b2799fb90422
LEE Toni <toni.lee@soprasteria.com>
2019-02-08 15:16:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Learning vue js
+
+https://www.smashingmagazine.com/2019/02/interactive-weather-dashboard-api-vue-js/
---
+++ src/components/Content.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="pagecontent"> <!--Position relative -->
+    <temp-var-chart :tempVar="tempVar" />
+    <today-highlights :highlights="highlights" />
+  </div>
+</template>
+
+<script>
+import TempVarChart from './TempVarChart.vue';
+import Highlights from './Highlights.vue';
+
+export default {
+  props: ["highlights", "tempVar"],
+  components: {
+      'temp-var-chart': TempVarChart,
+      'today-highlights': Highlights
+  }
+};
+</script>
+
+<style>
+</style>
+
